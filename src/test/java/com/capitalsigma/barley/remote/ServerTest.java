04028e1cbfc69d21393a565eac2a89a9b58112d5
pscollins <pscollins@uchicago.edu>
2015-07-14 15:43:04 -0500

    unit testing stuff in place

---
+++ src/test/java/com/capitalsigma/barley/remote/ServerTest.java
@@ -0,0 +1,17 @@
+package com.capitalsigma.barley.remote;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by patrick on 7/14/15.
+ */
+public class ServerTest {
+
+    @Test
+    public void testGetFrontPageSubverse() throws Exception {
+        assertTrue(false);
+
+    }
+}
